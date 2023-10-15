dd8b36fb12b993eb26b4510131c53fe381d47c77
Patronogenesys <Kvazar.vsz@ya.ru>
2023-10-15 18:22:32 +0300

    dz_4

---
+++ sem_6_dz_5/code.js
@@ -0,0 +1,11 @@
+let a = "Hello Иван";
+// alert(a);
+console.log(a);
+let but = document.getElementById("button");
+let answer = document.querySelector(".answer"); 
+
+function fun(){
+    let user_name = document.querySelector(".user_name").value;
+    let url = document.querySelector(".").value;
+    let comm = document.querySelector("#textbox").value;
+}
